d5b158ffb6336bd13185cf969ebc82b950a6884d
AymericPost <aymeric.post@gmail.com>
2021-03-19 09:45:38 +0100

    initial commit

---
+++ src/main/java/fr/inetum/web/controllers/ProductController.java
@@ -0,0 +1,23 @@
+package fr.inetum.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.inetum.web.models.Product;
+import fr.inetum.web.models.ProductRepository;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ProductController {
+
+	@Autowired
+	ProductRepository productRepository;
+	
+	@PostMapping("/product")
+	Product createProduct(@RequestBody Product newProduct) {
+		return productRepository.save(newProduct);
+	}
+}
---
+++ src/main/java/fr/inetum/web/models/ProductRepository.java
@@ -0,0 +1,7 @@
+package fr.inetum.web.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
